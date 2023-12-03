e7553345a007b7335f3ce260f89b0fade5a3a188
Eduardo Franklin Model <edu_model@hotmail.com>
2023-12-03 18:00:16 +0100

    chore(contact): added missing bootstrap classes

--- src/core/views/contact.php
+++ src/core/views/contact.php
@@ -1,17 +1,18 @@
 <h1>Contact</h1>
 
-<form action="" class="form">
-    <div class="form-group">
-        <label for="subject">Subject</label>
-        <input type="text" name="subject" id="subject">
+<form action="" method="post" class="form">
+    <div class="form-group mb-3">
+        <label class="form-label" for="subject">Subject</label>
+        <input type="text" name="subject" id="subject" class="form-control">
     </div>
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email">
+    <div class="form-group mb-3">
+        <label class="form-label" for="email">Email</label>
+        <input type="email" name="email" id="email" class="form-control">
     </div>
-    <div class="form-group">
-        <label for="subject-description">Subjects description</label>
-        <textarea name="subject-description" id="subject-description" cols="30" rows="10"></textarea>
+    <div class="form-group mb-3">
+        <label class="form-label" for="subject-description">Subjects description</label>
+        <textarea name="subject-description" id="subject-description" cols="30" rows="10"
+            class="form-control"></textarea>
     </div>
     <button class="btn btn-primary" type="submit">Submit</button>
 </form>
