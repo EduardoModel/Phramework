564d970fc635d1974c9128c26b801a93ce2b0814
Eduardo Franklin Model <edu_model@hotmail.com>
2024-01-06 09:30:39 +0100

    feat(Method): added basic enum with http verbs

--- src/core/form/Form.php
+++ src/core/form/Form.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Phramework\core\form;
 
+use Phramework\core\Http\Method;
+
 class Form
 {
-  public static function begin(string $action, string $method): string
+  public static function begin(string $action, Method $method): string
   {
-    return '<form action="'.$action.'" method="'.$method.'">';
+    return '<form action="'.$action.'" method="'.$method->value.'">';
   }
 
   public static function end(): string
